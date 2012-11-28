50b39c58e4295d1d25c1a686d7c33c92ffec9998
Tyler Renelle <tylerrenelle@gmail.com>
2012-11-28 17:18:33 -0500

    setup routes for details/edit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,25 +8,10 @@
 var habitrpg = angular.module('habitrpg', [])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider
-            .when('/habit', {
-                templateUrl: 'views/list.html'
-            })
-            .when('/daily', {
-                templateUrl: 'views/list.html'
-            })
-            .when('/todo', {
-                templateUrl: 'views/list.html'
-            })
-            .when('/todo/active', {
-                templateUrl: 'views/list.html'
-            })
-            .when('/todo/completed', {
-                templateUrl: 'views/list.html'
-            })
-            .when('/reward', {
-                templateUrl: 'views/list.html'
-            })
-            .otherwise({
-                redirectTo: '/habit'
-            });
+            .when('/*', {templateUrl: 'views/list.html'})
+            .when('/*/:taskId', {templateUrl: 'views/details.html'})
+            .when('/*/:taskId/edit', {templateUrl: 'views/details.html'})
+            .when('/todo/active', {templateUrl: 'views/list.html'})
+            .when('/todo/completed', {templateUrl: 'views/list.html'})
+            .otherwise({redirectTo: '/habit'});
     }]);
