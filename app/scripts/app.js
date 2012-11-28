fe16f95a2f39cb9406c49e699556b9e06983c5da
Tyler Renelle <tylerrenelle@gmail.com>
2012-11-28 12:12:17 -0500

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var mobileAngularApp = angular.module('mobileAngularApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
