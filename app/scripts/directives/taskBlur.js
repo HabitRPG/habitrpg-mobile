05cb0d8a9a4c9ab1820fddc63164fc176c583922
Tyler Renelle <tylerrenelle@gmail.com>
2013-03-11 20:23:43 -0400

    rename todo to task as necessary

--- app/scripts/directives/taskBlur.js
+++ app/scripts/directives/taskBlur.js
@@ -3,10 +3,10 @@
 /**
  * Directive that executes an expression when the element it is applied to loses focus.
  */
-habitrpg.directive('todoBlur', function() {
+habitrpg.directive('taskBlur', function() {
   return function( scope, elem, attrs ) {
     elem.bind('blur', function() {
-      scope.$apply(attrs.todoBlur);
+      scope.$apply(attrs.taskBlur);
     });
   };
 });
--- app/scripts/directives/taskFocus.js
+++ app/scripts/directives/taskFocus.js
@@ -3,9 +3,9 @@
 /**
  * Directive that places focus on the element it is applied to when the expression it binds to evaluates to true.
  */
-habitrpg.directive('todoFocus', function( $timeout ) {
+habitrpg.directive('taskFocus', function( $timeout ) {
   return function( scope, elem, attrs ) {
-    scope.$watch(attrs.todoFocus, function( newval ) {
+    scope.$watch(attrs.taskFocus, function( newval ) {
       if ( newval ) {
         $timeout(function() {
           elem[0].focus();
