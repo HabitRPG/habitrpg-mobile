b0ffe93bac0fd5e7184d538d50e7a6e82b0b8fcf
Neohuman <wesleympennock@gmail.com>
2013-03-25 10:28:34 -0500

    fixed per litenull's suggestion

--- app/scripts/services/authServices.js
+++ app/scripts/services/authServices.js
@@ -21,16 +21,14 @@ angular.module('authServices', ['userServices']).
             },
 
             login: function() {
-                user.id = 'fe4ebe85-137c-4e6a-b8a4-9d117b02310c';
-                user.apiToken = 'd805aaee-d74b-469c-ba16-d58e0b84e29a';
+                user.id = '';
+                user.apiToken = '';
                 User.authenticate();
                 return;
 
                 FB.login(function(response) {
                     if (response.authResponse) {
                         // 1. get userid & accesstoken
-                        var uid = response.authResponse.userID;
-                        var accessToken = response.authResponse.accessToken;
                         // 2. store in user
                         // 3. authenticate()
                         debugger;
@@ -70,9 +68,8 @@ angular.module('authServices', ['userServices']).
             },
 
             login: function() {
-                // fixme temporary hard-coded
-                user.id = '91dae4a1-895f-4698-a768-67ec0c8293bb';
-                user.apiToken = 'e984549d-6364-42eb-beec-1f075d80381d';
+                user.id = '';
+                user.apiToken = '';
                 User.authenticate();
                 return;
 
