aecdbef614cc3a56a579e74adb79a075c09ec0b9
al-kabeer <al-kabeer@alkabeer-HP-Pavilion-dm4-Notebook-PC.(none)>
2013-05-06 18:13:51 +0200

    fix task editing and enable syncing to server

--- app/scripts/controllers/taskDetailsCtrl.js
+++ app/scripts/controllers/taskDetailsCtrl.js
@@ -12,24 +12,26 @@ habitrpg.controller( 'TaskDetailsCtrl', function TaskDetailsCtrl( $scope, $rootS
   }
 
   $scope.edit = function() {
-      $scope.editedTask = _.clone($scope.task); // TODO deep clone?;
+      $scope.originalTask = _.clone($scope.task); // TODO deep clone?;
       $scope.editing = true;
   };
 
   $scope.save = function() {
-      $scope.task = $scope.editedTask;
-      User.save()
+      User.log({op: 'edit_task', task: $scope.task});
+      User.save();
+      $scope.originalTask = null;
       $scope.editing = false;
-      $scope.$apply()
   };
 
   $scope.cancel = function() {
-      $scope.editedTask = null;
+      $scope.task = $scope.originalTask;
+      $scope.originalTask = null;
       $scope.editing = false;
   }
 
   $scope.delete = function() {
       $scope.task.del = true;
+      User.log({op: 'delete_task', task: $scope.task.id});
       User.save()
       $scope.goBack();
   };
