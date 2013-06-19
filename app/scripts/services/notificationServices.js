9f258eddf4ff4e858a0c1e9d23cde8da5b2f0de7
yangit <yan@yan.my>
2013-06-19 10:16:24 +0800

    Refactor notification controller\service.

--- app/scripts/services/notificationServices.js
+++ app/scripts/services/notificationServices.js
@@ -1,80 +1,61 @@
 angular.module('notificationServices', []).
-	factory('Notification', function() {
+    factory('Notification', function () {
 
-		var delta  = { delta:0 };
-		var active = false
-		var timer = null
+        var delta = { delta: 0 };
+        var active = false;
+        var timer = null;
 
-		return {
+        return {
 
-			hide:function() {
+            hide: function () {
+                $('#notification').fadeOut(function () {
+                    $('#notification').css('top', '-60px').css('webkit-transform', 'none').show().css('left', '0px');
+                });
 
-				$('#notification').fadeOut(function() {
-				$('#notification').css('top','-60px');
-				$('#notification').css('webkit-transform', 'none')
-				$('#notification').show()
-				$('#notification').css('left', '0px')
-				});
-					
-				active		 = false;
-				timer 		 = null
-				delta.delta  = 0;
+                active = false;
+                timer = null;
+                delta.delta = 0;
+            },
 
-			},
+            animate: function () {
 
-			animate:function() {
+                if (timer) {
+                    clearTimeout(timer);
+                    timer = setTimeout(this.hide, 2000)
+                }
 
-				var that = this
+                if (active == false) {
+                    active = true;
 
+                    $('#notification').transition({ y: 60, x: 0 });
+                    timer = setTimeout(this.hide, 2000);
+                }
 
-				if (timer) {
-					clearTimeout(timer);
-					timer = setTimeout(this.hide, 2000)
-				}
+            },
 
-				if (active == false) {
-					active = true;
+            push: function (newDelta) {
+                delta.delta += newDelta;
+                this.animate()
+            },
 
-					$('#notification').transition({ y: 60, x:0 })
-					timer = setTimeout(this.hide, 2000)
+            get: function () {
+                return delta;
+            },
 
-				}
+            clearTimer: function () {
+                clearTimeout(timer);
+                timer = null;
+                active = false;
+            },
 
-			},
+            clearDelta: function () {
+                delta.delta = 0;
+            },
 
-			push:function(new_delta) {
+            init: function () {
+                timer = setTimeout(this.hide, 2000);
+            }
 
-				delta.delta += new_delta
-				this.animate()
+        }
 
-			},
-
-			get:function(back) {
-
-				return back(delta)
-
-			},
-
-			clearTimer:function() {
-
-				clearTimeout(timer);
-				timer = null;
-				active = false;
-
-			},
-
-			clearDelta:function() {
-
-				delta.delta = 0;
-
-			},
-
-			init:function() {
-
-				timer = setTimeout(this.hide, 2000)
-
-			}
-
-		}
-
-	});
+    });
