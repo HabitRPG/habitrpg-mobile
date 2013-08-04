d18d0f6da5de2ac4aed4deadaca4e21275dcbca7
Tyler Renelle <tylerrenelle@gmail.com>
2013-08-03 21:16:11 -0400

    remove unecessary home.html. explicitly declare task-type routes, they're messing with things. revisit this

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,15 +9,22 @@
 var habitrpg = angular.module('habitrpg', ['userServices', 'sharedServices', 'authServices', 'notificationServices', 'ngMobile'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
-            .when('/home', {templateUrl: 'views/home.html'})
-            .when('/login', {templateUrl: 'views/login.html'})
-            .when('/settings', {templateUrl: 'views/settings.html'})
-            .when('/profile', {templateUrl: 'views/profile.html'})
-            .when('/:action', {templateUrl: 'views/list.html'})
-            .when('/tasks/:taskId', {templateUrl: 'views/details.html'})
-            .when('/todo/active', {templateUrl: 'views/list.html'})
-            .when('/todo/completed', {templateUrl: 'views/list.html'})
-            .otherwise({redirectTo: '/habit'});
+
+            .when('/login', {templateUrl: '/views/login.html'})
+            .when('/settings', {templateUrl: '/views/settings.html'})
+            .when('/profile', {templateUrl: '/views/profile.html'})
+
+            .when('/tasks/:taskId', {templateUrl: '/views/details.html'})
+
+            .when('/habit', {templateUrl: '/views/list.html'})
+            .when('/daily', {templateUrl: '/views/list.html'})
+            .when('/todo', {templateUrl: '/views/list.html'})
+            .when('/reward', {templateUrl: '/views/list.html'})
+
+            .when('/todo/active', {templateUrl: '/views/list.html'})
+            .when('/todo/completed', {templateUrl: '/views/list.html'})
+
+            .otherwise({redirectTo: '/profile'});
     }])
     .config(['$compileProvider', function ($compileProvider) {
         $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
