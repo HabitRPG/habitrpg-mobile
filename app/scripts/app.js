25de3aadaf69571f4be001e01d7ae8117efce1b7
Tyler Renelle <tylerrenelle@gmail.com>
2013-08-03 20:36:04 -0400

    Upgrade to angular 1.1.5. remove gfTap, jquery.transit, jquery.swipe, hammer - these are all now taken care of in ngMobile (angular 1.1.4+). Move /scripts/vendor to /lib (a bit more traditional).

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,7 +6,7 @@
  * @type {angular.Module}
  */
 
-var habitrpg = angular.module('habitrpg', ['userServices', 'sharedServices', 'authServices', 'notificationServices', 'hmTouchEvents'])
+var habitrpg = angular.module('habitrpg', ['userServices', 'sharedServices', 'authServices', 'notificationServices', 'ngMobile'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
             .when('/home', {templateUrl: 'views/home.html'})
@@ -22,38 +22,6 @@ var habitrpg = angular.module('habitrpg', ['userServices', 'sharedServices', 'au
     .config(['$compileProvider', function ($compileProvider) {
         $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
     }]);
-
-
-// Touch directive, binding to touchstart as to not wait for 300ms
-
-habitrpg.directive('gfTap', ["$location", "$parse", function ($location, $parse) {
-    return function (scope, element, attrs) {
-        var tapping = false;
-        element.bind('touchstart', function () {
-            tapping = true;
-        });
-
-        element.bind('touchmove', function () {
-            tapping = false
-        });
-
-        element.bind('touchend', function (event) {
-            if (tapping) {
-                var fn = $parse(attrs['gfTap']);
-
-                if (attrs['href']) {
-                    var location = attrs['href'].replace('#', '/');
-                    $location.path(location);
-                }
-
-                scope.$apply(function () {
-                    fn(scope, {$event: event})
-                });
-            }
-        });
-
-    };
-}]);
 
 habitrpg.directive('sort', function (User) {
     return function ($scope, element, attrs, ngModel) {
