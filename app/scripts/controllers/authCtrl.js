facf6d9bd05f7190e903f43fc1bc0be1d4c9dd05
Tyler Renelle <tylerrenelle@gmail.com>
2013-08-11 17:56:18 -0400

    add ability login by uuid & token

--- app/scripts/controllers/authCtrl.js
+++ app/scripts/controllers/authCtrl.js
@@ -11,36 +11,50 @@ habitrpg.controller('AuthCtrl',
     $scope.Facebook = Facebook;
     $scope.Local = LocalAuth;
 
+    $scope.useUUID = false;
+    $scope.toggleUUID = function() {
+      $scope.useUUID = !$scope.useUUID;
+    }
+
     document.addEventListener('deviceready', function() {
 
-        FB.init({
-            appId: '374812825970494',
-            nativeInterface: CDV.FB,
-            useCachedDialogs: false
-        });
+      FB.init({
+        appId: '374812825970494',
+        nativeInterface: CDV.FB,
+        useCachedDialogs: false
+      });
 
     }, false);
 
     $scope.auth = function() {
-        var data = {
-            username: $scope.loginUsername,
-            password: $scope.loginPassword
-        }
+      var data = {
+        username: $scope.loginUsername,
+        password: $scope.loginPassword
+      }
 
-    $http.post(API_URL + '/api/v1/user/auth/local', data).success(function(data, status, headers, config) {
-            User.authenticate(data.id, data.token, function(err) {
-              alert('Login succesfull!');
-              $location.path("/habit");
-            });
+      var runAuth = function(id, token){
+        User.authenticate(id, token, function(err) {
+          alert('Login succesfull!');
+          $location.path("/habit");
+        });
+      }
 
-            // Angular 1.1.4 bug, see https://github.com/angular/angular.js/issues/2431#issuecomment-18160256
-            // Strange, it only crops up on initial login attempt
-            // if(!$rootScope.$$phase) {
-            //    $rootScope.$apply();
-            // }
-        }).error(function(data, status, headers, config) {
-            alert(status)
-    })
+      if ($scope.useUUID) {
+        debugger
+        return runAuth($scope.loginUsername, $scope.loginPassword);
+      }
+
+      $http.post(API_URL + '/api/v1/user/auth/local', data).success(function(data, status, headers, config) {
+        runAuth(data.id, data.token);
+
+        // Angular 1.1.4 bug, see https://github.com/angular/angular.js/issues/2431#issuecomment-18160256
+        // Strange, it only crops up on initial login attempt
+        // if(!$rootScope.$$phase) {
+        //    $rootScope.$apply();
+        // }
+      }).error(function(data, status, headers, config) {
+        alert(status)
+      })
 
     }
   }
