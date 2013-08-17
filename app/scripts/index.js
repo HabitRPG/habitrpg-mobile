1061a49968db5a05ffcb2e786d6495635600e92e
Tyler Renelle <tylerrenelle@gmail.com>
2013-08-17 12:56:56 -0400

    use cordova to bootstrap angular onDeviceReady. possible fix for 2.3 devices?

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,9 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 /* Possibly necessary stuff for Angular + Cordova. See:
  * 1) https://github.com/hollyschinsky/MyAngularPhoneGapProject/tree/master/www
  * 2) http://stackoverflow.com/questions/15105910/angular-ng-view-routing-not-working-in-phonegap
  * They differ, so we may need dto consolidate some stuff?
  */
+var app = {
+  // Application Constructor
+  initialize: function() {
+    this.bindEvents();
+  },
 
-document.addEventListener('backbutton', function(event) {
-	event.preventDefault()
-})
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function() {
+    document.addEventListener('load', this.onLoad, false);
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+    document.addEventListener('backbutton', function(event) {
+      event.preventDefault()
+    })
+  },
+
+  onLoad: function() {
+
+  },
+
+  // deviceready Event Handler
+  onDeviceReady: function() {
+    angular.element(document).ready(function() {
+      angular.bootstrap(document);
+    });
+  }
+};
