1d92f98a8bf0787de29c4b338b572b01d0e9bff9
Tyler Renelle <tylerrenelle@gmail.com>
2014-05-02 11:56:17 -0600

    feat(ads): remove ads for now, see #200

--- scripts/controllers/rootCtrl.js
+++ scripts/controllers/rootCtrl.js
@@ -2,53 +2,8 @@
 
 // Make user and settings available for everyone through root scope.
 habitrpg.controller('RootCtrl',
-  ['$scope', '$rootScope', '$location', 'User', '$state', '$stateParams', '$window', '$ionicPlatform',
-  function ($scope, $rootScope, $location, User, $state, $stateParams, $window, $ionicPlatform) {
-
-    var adsInited = false;
-    $rootScope.$on('userSynced', function(){
-
-      $ionicPlatform.ready(function() {
-        if( window.plugins && window.plugins.AdMob && !$ionicPlatform.is('iOS')) {
-          var am = window.plugins.AdMob;
-
-          // Remove ads / return if user is subscriber
-          if (User.user.purchased.plan.customerId || User.user.purchased.ads) {
-            if (adsInited) {
-              am.destroyBannerView();
-              adsInited = false;
-            }
-            return;
-          }
-          if (adsInited) return;
-          adsInited = true;
-
-          var admob_ios_key = ADMOB_IOS_KEY;
-          var admob_android_key = ADMOB_ANDROID_KEY;
-          var adId = (navigator.userAgent.indexOf('Android') >=0) ? admob_android_key : admob_ios_key;
-
-          am.createBannerView(
-            {
-              'publisherId': adId,
-              'adSize': am.AD_SIZE.BANNER,
-              'bannerAtTop': true
-            },
-            function() {
-              am.requestAd(
-                { 'isTesting':false },
-                function(){
-                  am.showAd( true );
-                },
-                function(){ console.error('failed to request ad'); }
-              );
-            },
-            function(){ console.error('failed to create banner view'); }
-          );
-        } else {
-          console.error('AdMob plugin not available/ready.');
-        }
-      });
-    });
+  ['$scope', '$rootScope', '$location', 'User', '$state', '$stateParams', '$window',
+  function ($scope, $rootScope, $location, User, $state, $stateParams, $window) {
 
     $rootScope.User = User;
     $rootScope.user = User.user;
