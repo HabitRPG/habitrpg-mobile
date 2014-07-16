a69b772d08605f3630dc0f4f221d4fdb42285360
huaruiwu <huarui.wu@gmail.com>
2014-07-16 01:56:52 -0700

    update

--- scripts/controllers/tasksCtrl.js
+++ scripts/controllers/tasksCtrl.js
@@ -45,7 +45,8 @@ habitrpg
       User.user.ops.sortTask({params:{id:task.id},query:{from:fromIndex, to:toIndex}});
     };
 
-    $scope.score = function (task, direction) {
+    $scope.score = function (task, direction, e) {
+      if (e) e.stopPropagation();
       //save current stats to compute the difference after scoring.
       var statsDiff = {};
       var oldStats = _.clone(User.user.stats);
@@ -95,13 +96,15 @@ habitrpg
         //To be implemented
     };
 
-    $scope.changeCheck = function (task) {
+    $scope.changeCheck = function (task, e) {
         // This is calculated post-change, so task.completed=true if they just checked it
+        if (e) e.stopPropagation();
         if (task.completed) {
             $scope.score(task, 'up')
         } else {
             $scope.score(task, 'down')
         }
+        task.completed = !task.completed;
     }
 
     $scope.actionSheet = function(task) {
@@ -120,6 +123,10 @@ habitrpg
         }
       });
     };
+
+    $scope.viewTask = function(task, e) {
+      $state.go('app.task',{tid:task.id});
+    }
 
 
     /**
