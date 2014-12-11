2dfb3a437ea11ca06e11520fe2017e0382d534f7
Negue <eugen.bolz@gmail.com>
2014-12-11 23:49:51 +0100

    Add Alerts for iOS and custom Server URL

--- scripts/controllers/purchaseCtrl.js
+++ scripts/controllers/purchaseCtrl.js
@@ -1,11 +1,21 @@
 'use strict';
-console.log("beforeControllerAdd");
+
 habitrpg.controller('PurchaseCtrl', 
-  ['$scope', 'StoreService',
-  function($scope, store){
+  ['$scope', 'StoreService', '$rootScope', 'ApiUrlService',
+  function($scope, store, $rootScope, ApiUrlService){
     $scope.store = store.getStore();
 
     $scope.buy = function(id){
+      if($rootScope.isIOS){
+        alert("In-App-Purchases are currently not available on IOS.");
+        return;
+      }
+
+      if(ApiUrlService.get() !== 'https://habitrpg.com'){
+        alert("You are using a custom server, please contact your server administrator.");
+        return;
+      }
+
       try{
         $scope.store.order(id)
           .then(function(ex){
