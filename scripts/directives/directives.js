bb8f88d06eaf0d27b54d06c6c70ce377efb00493
Negue <eugen.bolz@gmail.com>
2015-01-19 17:13:10 +0100

    Change the Color of Habits

--- scripts/directives/directives.js
+++ scripts/directives/directives.js
@@ -6,12 +6,44 @@ angular.module('habitrpg')
       var isDaily = attrs.taskList == 'daily';
       var isTodo = attrs.taskList == 'todo';
 
+      var CHECKED_CLASS = 'icon ion-checkmark-circled';
+      var UNCHECKED_CLASS = 'checkbox-icon';
+
+      function changeIonItemColor(task, ionItem){
+        var removeColorClass = undefined;
+        var classList = ionItem.className.split(' ');
+
+        for (var i in classList) {
+          var className = classList[i];
+
+          if (className.indexOf("color-") != -1) {
+            removeColorClass = className;
+            break;
+          }
+        }
+
+        if (removeColorClass != undefined)
+          ionItem.classList.remove(removeColorClass);
+
+        // add new / changed classes
+
+        var newColorClass = scope.getClass(task.value).trim();
+
+        ionItem.classList.add(newColorClass);
+      }
+
+      scope.changeColor = function (task, $event) {
+        var btn = $event.target;
+
+        // yes this looks ugly, but jqLite doesn't support selectors :/
+        var ionItem = btn.parentNode.parentNode.parentNode.parentNode;
+
+        changeIonItemColor(task, ionItem);
+      };
+
       if(!isDaily && !isTodo) {
         return;
       }
-
-      var CHECKED_CLASS = 'icon ion-checkmark-circled';
-      var UNCHECKED_CLASS = 'checkbox-icon';
 
       scope.changeCheck = function(task, $event){
         var btn = $event.target;
@@ -21,7 +53,6 @@ angular.module('habitrpg')
         var btnIcon = angular.element(btn).find('i')[0];
 
         // default changeCheck behavior
-
         if ($event) $event.stopPropagation();
         task.completed = !task.completed;
         if (task.completed) {
@@ -33,28 +64,9 @@ angular.module('habitrpg')
         ionItem.classList.remove("completed");
 
         // find the "current" color class & remove it
-
-        var removeColorClass = undefined;
-        var classList = ionItem.className.split(' ');
-
-        for(var i in classList){
-          var className  = classList[i];
-
-          if(className.indexOf("color-") != -1){
-            removeColorClass = className;
-            break;
-          }
-        }
-
-        if(removeColorClass != undefined)
-          ionItem.classList.remove(removeColorClass);
+        changeIonItemColor(task, ionItem);
 
         // add new / changed classes
-
-        var newColorClass = scope.getClass(task.value).trim();
-
-        ionItem.classList.add(newColorClass);
-
         if(task.completed || scope.notDue(task))
         {
           ionItem.classList.add("completed");
