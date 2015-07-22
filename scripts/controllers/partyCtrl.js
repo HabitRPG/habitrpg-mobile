fff626f156b5d2c0cd8a7bc1d134e87e3f88dd31
Blade Barringer <blade@crookedneighbor.com>
2015-07-21 20:04:03 -0500

    Fix quest accept and quest reject buttons

--- scripts/controllers/partyCtrl.js
+++ scripts/controllers/partyCtrl.js
@@ -5,6 +5,14 @@ habitrpg.controller('PartyCtrl',
   function($scope, $rootScope) {
     $scope.group = $rootScope.party;
 
+    $scope.questAccept = function(party) {
+      party.$questAccept();
+    }
+
+    $scope.questReject = function(party) {
+      party.$questReject();
+    }
+
     $scope.questAbort = function(){
       if(!confirm(window.env.t('sureAbort'))) return;
       if(!confirm(window.env.t('doubleSureAbort'))) return;
